5257a5a01a4144e03209a67ced97af3412326c17
Rakibul Islam <rakibul4516>
2023-10-19 19:43:25 +0600

    this commit adds delete method to delete cart to product

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// rakibulislam4516
-// 0adHVAEKWH12N599
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ufdhagf.mongodb.net/?retryWrites=true&w=majority`;
@@ -91,7 +90,12 @@ async function run() {
 
         //Delete method
 
-
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartsDatabase.deleteOne(query);
+            res.send(result)
+        })
         //Put method
 
 
